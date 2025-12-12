24514c1db79e48e9a12c3a44ac7d6093b0198d64
ace1110 <liaojiayouy@gmail.com>
2025-12-12 14:40:59 +0800

    Auto commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# add the file here
+
+from random import randint
+from enum import Enum
+
+class Colors(Enum):
+    Red = 0
+    Green = 1
+    Blue = 2
+    Orange = 3
+    Purple = 4
+
+grid: list[list[Colors|int]] = [[randint(0, 5) for _ in range(6)] for __ in range(6)]
